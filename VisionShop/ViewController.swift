7c8dd0193bf8812c134540483e5b9eb02d92e797
kjwamlex <Joonwoo@iMac.local>
2017-08-05 16:06:28 -0400

    Initial Commit

---
+++ VisionShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionShop
+//
+//  Created by 김준우 on 2017-08-05.
+//  Copyright © 2017 김준우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
